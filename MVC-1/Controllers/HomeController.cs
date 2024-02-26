6575e06959c6675cabe5ca52e433bac89643ac43
Omar Lo Giudice <146293874+FearXz@users.noreply.github.com>
2024-02-26 19:35:05 +0100

    fatto detail

--- MVC-1/Controllers/HomeController.cs
+++ MVC-1/Controllers/HomeController.cs
@@ -172,5 +172,156 @@ namespace MVC_1.Controllers
             }
             return View(pagamenti);
         }
+
+        public ActionResult DeleteDipendente(int id)
+        {
+            string connectionString = ConfigurationManager.ConnectionStrings["MyDb"].ToString();
+            SqlConnection conn = new SqlConnection(connectionString);
+
+            try
+            {
+                conn.Open();
+
+                string query = "DELETE FROM Dipendenti WHERE IDDipendente = @IDDipendente";
+
+                SqlCommand cmd = new SqlCommand(query, conn);
+                cmd.Parameters.AddWithValue("@IDDipendente", id);
+
+                cmd.ExecuteNonQuery();
+            }
+            catch (Exception ex)
+            {
+                Response.Write($"Errore durante il recupero dei dati: {ex.Message}");
+            }
+            finally
+            {
+                conn.Close();
+            }
+
+            return RedirectToAction("Index");
+        }
+
+        public ActionResult EditDipendente(int id)
+        {
+            string connectionString = ConfigurationManager.ConnectionStrings["MyDb"].ToString();
+            SqlConnection conn = new SqlConnection(connectionString);
+
+            Dipendente dipendente = new Dipendente();
+
+            try
+            {
+                conn.Open();
+
+                string query = "SELECT * FROM Dipendenti WHERE IDDipendente = @IDDipendente";
+
+                SqlCommand cmd = new SqlCommand(query, conn);
+                cmd.Parameters.AddWithValue("@IDDipendente", id);
+
+                SqlDataReader reader = cmd.ExecuteReader();
+
+                while (reader.Read())
+                {
+                    dipendente.IDDipendente = Convert.ToInt32(reader["IDDipendente"]);
+                    dipendente.Nome = reader["Nome"].ToString();
+                    dipendente.Cognome = reader["Cognome"].ToString();
+                    dipendente.Indirizzo = reader["Indirizzo"].ToString();
+                    dipendente.CodiceFiscale = reader["CodiceFiscale"].ToString();
+                    dipendente.Mansione = reader["Mansione"].ToString();
+                    dipendente.Coniugato = Convert.ToBoolean(reader["Coniugato"]);
+                    dipendente.NFigliACarico = Convert.ToInt32(reader["NFigliACarico"]);
+                }
+            }
+            catch (Exception ex)
+            {
+                Response.Write($"Errore durante il recupero dei dati: {ex.Message}");
+            }
+            finally
+            {
+                conn.Close();
+            }
+
+            return View(dipendente);
+        }
+
+        [HttpPost]
+        public ActionResult EditDipendente(Dipendente d)
+        {
+            string connectionString = ConfigurationManager.ConnectionStrings["MyDb"].ToString();
+            SqlConnection conn = new SqlConnection(connectionString);
+
+            try
+            {
+                conn.Open();
+
+                string query =
+                    "UPDATE Dipendenti SET Nome = @Nome, Cognome = @Cognome, Indirizzo = @Indirizzo, CodiceFiscale = @CodiceFiscale, Mansione = @Mansione, Coniugato = @Coniugato, NFigliACarico = @NFigliACarico WHERE IDDipendente = @IDDipendente";
+
+                SqlCommand cmd = new SqlCommand(query, conn);
+                cmd.Parameters.AddWithValue("@IDDipendente", d.IDDipendente);
+                cmd.Parameters.AddWithValue("@Nome", d.Nome);
+                cmd.Parameters.AddWithValue("@Cognome", d.Cognome);
+                cmd.Parameters.AddWithValue("@Indirizzo", d.Indirizzo);
+                cmd.Parameters.AddWithValue("@CodiceFiscale", d.CodiceFiscale);
+                cmd.Parameters.AddWithValue("@Mansione", d.Mansione);
+                cmd.Parameters.AddWithValue("@Coniugato", d.Coniugato);
+                cmd.Parameters.AddWithValue("@NFigliACarico", d.NFigliACarico);
+
+                cmd.ExecuteNonQuery();
+            }
+            catch (Exception ex)
+            {
+                Response.Write($"Errore durante il recupero dei dati: {ex.Message}");
+            }
+            finally
+            {
+                conn.Close();
+            }
+
+            return RedirectToAction("Index");
+        }
+
+        public ActionResult DetailDipendente(int id)
+        {
+            string connectionString = ConfigurationManager.ConnectionStrings["MyDb"].ToString();
+            SqlConnection conn = new SqlConnection(connectionString);
+
+            List<DetailDipendente> listaDipendenti = new List<DetailDipendente>();
+
+            try
+            {
+                conn.Open();
+
+                string query =
+                    "SELECT d.IDDipendente, d.Nome,d.Cognome,p.DataPagamento,p.Ammontare,p.Tipo FROM Dipendenti AS d INNER JOIN Pagamenti AS p ON d.IDDipendente = p.IDDipendente WHERE d.IDDipendente = @IDDipendente";
+
+                SqlCommand cmd = new SqlCommand(query, conn);
+                cmd.Parameters.AddWithValue("@IDDipendente", id);
+
+                SqlDataReader reader = cmd.ExecuteReader();
+
+                while (reader.Read())
+                {
+                    DetailDipendente dipendente = new DetailDipendente();
+                    dipendente.IDDipendente = Convert.ToInt32(reader["IDDipendente"]);
+                    dipendente.Nome = reader["Nome"].ToString();
+                    dipendente.Cognome = reader["Cognome"].ToString();
+                    dipendente.DataPagamento = Convert.ToDateTime(reader["DataPagamento"]);
+                    dipendente.Ammontare = Convert.ToDecimal(reader["Ammontare"]);
+                    dipendente.Tipo = reader["Tipo"].ToString();
+
+                    listaDipendenti.Add(dipendente);
+                }
+            }
+            catch (Exception ex)
+            {
+                Response.Write($"Errore durante il recupero dei dati: {ex.Message}");
+            }
+            finally
+            {
+                conn.Close();
+            }
+
+            return View(listaDipendenti);
+        }
     }
 }
---
+++ MVC-1/Models/DetailDipendente.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace MVC_1.Models
+{
+    public class DetailDipendente
+    {
+        public int IDDipendente { get; set; }
+        public string Nome { get; set; }
+        public string Cognome { get; set; }
+        public DateTime DataPagamento { get; set; }
+        public decimal Ammontare { get; set; }
+        public string Tipo { get; set; }
+    }
+}
